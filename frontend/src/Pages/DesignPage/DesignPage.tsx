ac59d469596f3cfe285a20895f90ec7cba3d359e
augustocordeiro663-lang <augustocordeiro663@gmail.com>
2026-05-11 22:40:00 -0300

    Table

---
+++ frontend/src/Pages/DesignPage/DesignPage.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Table from '../../Components/Table/Table';
+
+interface Props  {}
+
+const DesignPage = (props: Props) => {
+  return (
+    <>
+    <h1>FinShark Desing Page</h1>
+    <h2>This is FinShark's design page. This is where we will house various desing aspects of the app  
+    </h2>
+    <Table /> 
+    </>
+  );
+
+};
+export default DesignPage;
--- frontend/src/Routes/Routes.tsx
+++ frontend/src/Routes/Routes.tsx
@@ -3,6 +3,7 @@ import HomePage from "../Pages/HomePage/HomePage";
 import SearchPage from "../Pages/SearchPage/SearchPage";
 import CompanyPage from "../Pages/CompanyPage/CompanyPage";
 import App from "../App";
+import DesignPage from "../Pages/DesignPage/DesignPage";
 
 export const router = createBrowserRouter([
   {
@@ -11,6 +12,7 @@ export const router = createBrowserRouter([
     children: [
       { path: "", element: <HomePage /> },
       { path: "search", element: <SearchPage /> },
+       { path: "design-guide", element: <DesignPage /> },
       {
         path: "company/:ticker",
         element: <CompanyPage />,
