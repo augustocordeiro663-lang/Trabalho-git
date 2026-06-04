bd30f41d3cd9d67aafd7921a5c4535c97c763663
augustocordeiro663-lang <augustocordeiro663@gmail.com>
2026-06-04 16:39:25 -0300

    POST (Create)

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -1,4 +1,5 @@
 using api.Data;
+using api.Dtos.Stock;
 using api.Mappers;
 using Microsoft.AspNetCore.Mvc;
 
@@ -37,9 +38,15 @@ namespace api.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create ([FromBody] CreateStockRequest stockDto)
+        public IActionResult Create ([FromBody] CreateStockRequestDto stockDto)
         {
+             var stockModel = stockDto.ToStockFromCreateDTO();
+             _context.Stocks.Add(stockModel);
+             _context.SaveChanges();
+
             
+
+            return CreatedAtAction(nameof(GetById), new { id = stockModel.Id }, stockModel.ToStockDto());
         }
     }
 }
