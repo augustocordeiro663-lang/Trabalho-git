8acd374e03bbade876adf7d3168f9ac7b36e8027
augustocordeiro663-lang <augustocordeiro663@gmail.com>
2026-06-05 14:40:57 -0300

    Comment System na api

--- api/obj/Debug/net10.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net10.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a500432caacc41a81a28d597ff879f27d858455b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acf9f209fbe30b31bc315915a2cf83661e9ea9c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
